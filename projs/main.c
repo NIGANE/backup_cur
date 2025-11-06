47d37c3be4154016994db64c954b5fedc395530c
Achraf Merkht <amerkht@c4r3p8.1337.ma>
2025-11-06 11:53:01 +0100

    conf

---
+++ projs/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+
+int main(void)
+{
+    printf("short: %zu\n", sizeof(short));
+    printf("bool: %zu\n", sizeof(bool));
+    printf("char: %zu\n", sizeof(char));
+    printf("int: %zu\n", sizeof(int));
+    printf("long: %zu\n", sizeof(long));
+    printf("long long: %zu\n", sizeof(long long));
+    printf("float: %zu\n", sizeof(float));
+    printf("double: %zu\n", sizeof(double));
+    printf("long double: %zu\n", sizeof(long double));
+    
+
+}
