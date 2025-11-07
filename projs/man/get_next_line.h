160d5de8228cb74e061dbd5393ad96aaccdb1435
Achraf Merkht <amerkht@c4r2p7.1337.ma>
2025-11-07 15:59:27 +0100

    ending the mandatory part

---
+++ projs/man/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amerkht <amerkht@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 15:53:02 by amerkht           #+#    #+#             */
+/*   Updated: 2025/11/07 15:53:14 by amerkht          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000000
+# endif
+
+size_t	ft_strlen(char *s);
+int		ft_strchr(char *s, int c);
+char	*extract_line(const char *s);
+char	*ft_strjoin(char *s, char *b);
+char	*get_next_line(int fd);
+
+#endif
