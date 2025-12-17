fecd58d4488bcf39ce5159314e43b62201b062f3
Achraf Merkht <amerkht@c4r4p5.1337.ma>
2025-12-17 21:32:30 +0100

    NEGANE

---
+++ projs/includes/op.h
@@ -0,0 +1,13 @@
+#include "sta1.h"
+
+void sa(t_stack *a);
+void sb(t_stack *b);
+void ss(t_stack *a, t_stack *b);
+void pa(t_stack *a, t_stack *b);
+void pb(t_stack *a, t_stack *b);
+void ra(t_stack *stack);
+void rb(t_stack *stack);
+void rr(t_stack *a, t_stack *b);
+void rra(t_stack *stack);
+void rrb(t_stack *stack);
+void rrr(t_stack *a, t_stack *b);
--- projs/includes/sta1.h
+++ projs/includes/sta1.h
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <unistd.h>
 
 
 typedef struct s_stack_node
@@ -15,15 +16,21 @@ typedef struct stack
     int size;
 } t_stack;
 
+
 t_stack *stack_init();
 void free_stack(t_stack *stack);
 t_stack_node *create_stack(int data);
 t_stack *push(t_stack *stack, int data);
 t_stack_node *pop(t_stack *stack);
 void stack_rev(t_stack_node **top);
+int in_stack(int a, t_stack *st);
 void swap(t_stack_node **top);
 void rotate(t_stack *stack);
 void rev_rotate(t_stack *stack);
 void print_stack(t_stack *stack);
 t_stack *extract_stack(t_stack *st, int cn, char **av);
-int ft_atoi(char *str);
+long ft_atoi(char *str);
+char	**ft_split(char const *s, char c);
+void free_split_arr(char **arr);
+int ft_putstr(char *s);
+size_t ft_strlen(char *s);
