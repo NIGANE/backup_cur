3946f69a0de105bac6fc83859eeda25e0c09aac3
NIGANE <merkhtachraf163@gmail.com>
2026-01-07 17:18:25 +0100

    neg

---
+++ projs/ex1/ft_plot_area.py
@@ -0,0 +1,6 @@
+
+def ft_plot_area():
+    len = input("Enter length: ")
+    wid = input("Enter width: ")
+    calc = len + wid
+    print("Plot area: ${calc}")
---
+++ projs/main.py
@@ -0,0 +1,5 @@
+from ex0.ft_hello_garden import ft_hello_garden
+from ex1.ft_plot_area import ft_plot_area
+
+
+ft_plot_area()
