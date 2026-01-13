de9a22a11f6c25a6d5d19acf3d15fda2d77cd17d
Achraf Merkht <amerkht@c4r2p4.1337.ma>
2026-01-13 23:37:04 +0100

    NEGANE

---
+++ projs/main.py
@@ -0,0 +1,14 @@
+import pdb; 
+pdb.pm()
+def main():
+    breakpoint()
+    print("hello orld")
+    x = [1, 2, 3].__str__()
+    print(x)
+    print(type(int))
+    print(int)
+
+
+if __name__ == "__main__":
+    main()
+    pass
