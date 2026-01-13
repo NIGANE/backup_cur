ad0fb7b25ab45d196077b40b88de61d55e94c665
Achraf Merkht <amerkht@c4r2p4.1337.ma>
2026-01-13 23:47:04 +0100

    NEGANE

--- projs/ex2/ft_plant_growth.py
+++ projs/ex2/ft_plant_growth.py
@@ -3,10 +3,10 @@ class Plant:
         self.name = name
         self.height = height
         self.age = age
-    def grow(self, cm):
-        self.height += cm
-    def age(self, d):
-        self.age += d
+    def grow(self,):
+        self.height += 0.81
+    def age(self):
+        self.age += 1
     def get_info(self):
         return f"{self.name}: {self.height}cm, {self.age} days old"
     
@@ -15,7 +15,14 @@ def main():
     print("=== Day 1 ===")
     rosee = Plant("rose", 25, 30)
     print(rosee.get_info())
-    rosee.grow()
+    i = 1
+    while (i < 7):
+        rosee.grow()
+        rosee.age()
+        i += 1
+    print("=== Day 7 ===")
+    print(rosee.get_info())
+    print(f"Growth this week: {i * 0.81}cm")
 
 
 if __name__ == "__main__":
