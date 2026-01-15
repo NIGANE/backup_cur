f75b79218513d24d37ca962c0e79a9b3efbe8716
Achraf Merkht <amerkht@c4r2p4.1337.ma>
2026-01-15 12:37:17 +0100

    NEGANE

--- projs/ex1/ft_garden_data.py
+++ projs/ex1/ft_garden_data.py
@@ -1,12 +1,39 @@
 
 class Plant:
+    """
+        Represents a plant with basic attributes.
+
+        Attributes
+        ----------
+        name : str
+            Name of the plant.
+        height : int
+            Height of the plant in centimeters.
+        age : int
+            Age of the plant in days.
+    """
     def __init__(self, name: str, height: int, age: int):
+        """
+            Initialize a Plant instance.
+
+            Parameters
+            ----------
+            name : str
+                Name of the plant.
+            height : int
+                Height of the plant in centimeters.
+            age : int
+                Age of the plant in days.
+        """
         self.name = name
         self.height = height
         self.age = age
 
 
 def main():
+    """
+        Create plant instances and display their information.
+    """
     rosee = Plant("rose", 25, 30)
     Sunflower = Plant("sunflower", 80, 45)
     Cactus = Plant("cactus", 15, 120)
