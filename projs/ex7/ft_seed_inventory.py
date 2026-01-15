8d5600f8bdff4a89c0dc83df12a327c79c6b496a
Achraf Merkht <amerkht@c4r2p4.1337.ma>
2026-01-15 11:17:29 +0100

    NEGANE

--- projs/ex7/ft_seed_inventory.py
+++ projs/ex7/ft_seed_inventory.py
@@ -1,6 +1,16 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     txt = seed_type.capitalize()
-    if (unit not in ["packets", "grams", "area"]):
+    if (unit == "area"):
+        unit = "square meters"
+    elif (unit == "grams"):
+        unit = "grams total"
+    elif (unit == "packets"):
+        unit = "packets available"
+    else :
         print("Unknown unit type")
         return
-    print(f"{txt} seeds: {quantity} {unit} packets available")
+    print(f"{txt} seeds: {quantity} {unit}")
+
+ft_seed_inventory("tomato", 15, "packets")
+ft_seed_inventory("carrot", 8, "grams")
+ft_seed_inventory("lettuce", 12, "area")
