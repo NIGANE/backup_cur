f9585ff6fe3ed57ee5cb2b378f45a394c9d7cb68
Achraf Merkht <amerkht@c4r2p7.1337.ma>
2026-01-16 23:22:41 +0100

    NEGANE

--- projs/ex2/ft_coordinate_system.py
+++ projs/ex2/ft_coordinate_system.py
@@ -1,19 +1,19 @@
 import math
 
 
-def parsing_cordinates(data: str):
+def parsing_cordinates(data: str) -> tuple[int, int, int]:
     storing = []
     try:
         for ele in data.split(','):
-            storing.append(float(ele))
+            storing.append(int(ele))
         return (storing[0], storing[1], storing[2])
     except ValueError as e:
+        print(f"Parsing invalid coordinates: \"{data}\"")
         print(f"Error parsing coordinates: {e}")
         print(f"Error details - Type: {type(e).__name__}, Args: {e.args}")
 
 
 def ft_coordinate_system(data: tuple[int]) -> None:
-    print(f"Position created: {data}")
     x2, y2, z2 = data
     x1, y1, z1 = (0, 0, 0)
     diff = math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
@@ -21,13 +21,16 @@ def ft_coordinate_system(data: tuple[int]) -> None:
 
 
 def unpacke(data: tuple[int]) -> None:
+    print(f"Playser at x={data[0]}, y={data[1]}, z={data[2]}")
     print(f"Coordinates: X={data[0]}, Y={data[1]}, Z={data[2]}")
 
 
 def main() -> None:
     print("=== Game Coordinate System ===")
     print("")
-    ft_coordinate_system((10, 20, 5))
+    data = (10, 20, 5)
+    print(f"Position created: {data}")
+    ft_coordinate_system(data)
     print("")
     str_to_parse = "3,4,0"
     print(f"Parsing coordinates: \"{str_to_parse}\"")
@@ -36,12 +39,10 @@ def main() -> None:
     ft_coordinate_system(tp)
     print("")
     bad_str_parse = "abc,def,ghi"
-    print(f"Parsing invalid coordinates: {bad_str_parse}")
     tp = parsing_cordinates(bad_str_parse)
     print("")
     print("Unpacking demonstration:")
     p = (3, 4, 0)
-    print(f"playser at x={p[0]}, y={p[1]}, z={p[2]}")
     unpacke(p)
 
 
