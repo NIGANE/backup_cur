6963edc35f4f9935a375143e458747c41098f0f0
NIGANE <merkhtachraf163@gmail.com>
2026-02-11 15:08:50 +0100

    pushing updates

--- projs/ex0/space_station.py
+++ projs/ex0/space_station.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel, field_validator, ValidationError, Field
+from pydantic import BaseModel, ValidationError, Field
 from typing import Optional
-from datetime import datetime
+import datetime
 
 
 class SpaceStation(BaseModel):
@@ -14,54 +14,65 @@ class SpaceStation(BaseModel):
         max_length=10,
         description="name should be between (1-50) characters"
         )
-    crew_size: int
-    power_level: float
-    oxygen_level: float
-    last_maintenence: datetime
+    crew_size: int = Field(
+        ge=1,
+        le=20,
+        description="Input should be less than or equal to 20"
+    )
+    power_level: float = Field(
+        ge=0.0,
+        le=100.0,
+        description="power level should be between (0-100)"
+    )
+    oxygen_level: float = Field(
+        ge=0.0,
+        le=100.0,
+        description="oxygen level should be between (0-100)"
+    )
+    last_maintenence: datetime = Field(
+        description="input should be valid datetime"
+    )
     is_operational: bool = True
-    notes: Optional[str]
-
-    # @field_validator('crew_size')
-    # @classmethod
-    # def validate_crew_size(cls, v: int) -> int:
-    #     if (v > 20 or v < 1):
-    #         raise ValueError("must use size between (1-20)")
-    #     else:
-    #         return v
-
-    # @field_validator('power_level')
-    # @classmethod
-    # def validate_power_level(cls, v: float) -> float:
-    #     if (v > 100.0 or v < 0.0):
-    #         raise ValueError("must use power level between (1-20)")
-    #     else:
-    #         return v
-
-    # @field_validator('oxygen_level')
-    # @classmethod
-    # def validate_oxygen_level(cls, v: float) -> float:
-    #     if (v > 100.0 or v < 0.0):
-    #         raise ValueError("must use size between (1-20)")
-    #     else:
-    #         return v
-
-    # @field_validator('notes')
-    # @classmethod
-    # def validate_notes(cls, v: str) -> str:
-    #     if (len(v) > 200):
-    #         raise ValueError("must use string between (0 - 200) characters")
-    #     else:
-    #         return v
+    notes: Optional[str] = Field(
+        max_length=200,
+        description="notes should be less than or equal 200"
+    )
 
 
-def main() -> None:
-    print("Space Station Data Validation")
-    print("========================================")
-    station = None
-    import datetime
-    print(datetime.datetime.now())
-    # try:
-    #     station = SpaceStation(station_id="ISS001", name="International Space Station", crew_size=6, power_level=85.5, oxygen_level=92.3, is_operational=True, last)
+def output_mess(station) -> None:
+    print("Valid station created")
+    print(f"ID: {station.station_id}")
+    print(f"Name: {station.name}")
+    print(f"Crew: {station.crew_size}")
+    print(f"Power: {station.power_level}")
+    print(f"Oxygen: {station.oxygen_level}")
+    print(
+          f"Status: "
+          f"{"Operational" if station.is_operational else "Not operational"}"
+          )
 
 
-main() if __name__ == "__main__" else ""
+# def main() -> None:
+#     print("Space Station Data Validation")
+#     print("========================================")
+#     station = None
+#     now = datetime.datetime.now()
+#     try:
+#         station = SpaceStation(
+#             station_id="ISS001",
+#             name="International Space Station",
+#             crew_size=6,
+#             power_level=85.5,
+#             oxygen_level=92.3,
+#             last_maintenence=now,
+#             is_operational=True,
+#             notes=""
+#             )
+#     except ValidationError as e:
+#         print(e)
+#     else:
+#         output_mess(station)
+
+
+
+# main() if __name__ == "__main__" else ""
