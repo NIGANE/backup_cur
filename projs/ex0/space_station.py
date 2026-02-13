31d22ece409bac6cab2ca5bcdee043eebac9283e
NIGANE <merkhtachraf163@gmail.com>
2026-02-13 12:40:07 +0100

    pushing updates

--- projs/ex0/space_station.py
+++ projs/ex0/space_station.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, ValidationError, Field
 from typing import Optional
-import datetime
+from datetime import datetime
 
 
 class SpaceStation(BaseModel):
@@ -10,8 +10,8 @@ class SpaceStation(BaseModel):
         description="station id should be between (3-10) characters"
         )
     name: str = Field(
-        min_length=3,
-        max_length=10,
+        min_length=1,
+        max_length=50,
         description="name should be between (1-50) characters"
         )
     crew_size: int = Field(
@@ -44,8 +44,8 @@ def output_mess(station: SpaceStation) -> None:
     print(f"ID: {station.station_id}")
     print(f"Name: {station.name}")
     print(f"Crew: {station.crew_size}")
-    print(f"Power: {station.power_level}")
-    print(f"Oxygen: {station.oxygen_level}")
+    print(f"Power: {station.power_level}%")
+    print(f"Oxygen: {station.oxygen_level}%")
     print(
           f"Status: "
           f"{"Operational" if station.is_operational else "Not operational"}"
@@ -56,7 +56,7 @@ def main() -> None:
     print("Space Station Data Validation")
     print("========================================")
     station = None
-    now = datetime.datetime.now()
+    now = datetime.now()
     try:
         station = SpaceStation(
             station_id="ISS001",
@@ -76,20 +76,21 @@ def main() -> None:
     print("========================================")
     print("Expected validation error:")
     fault_station = None
-    now = datetime.datetime.now()
+    now = datetime.now()
     try:
         fault_station = SpaceStation(
             station_id="ISS001",
             name="International Space Station",
-            crew_size=6,
+            crew_size=56,
             power_level=85.5,
             oxygen_level=92.3,
             last_maintenence=now,
             is_operational=True,
             notes=""
             )
+        print(fault_station)
     except ValidationError as e:
-        print(e)
+        print(e.errors()[0]['msg'])
 
 
-# main() if __name__ == "__main__" else ""
+main() if __name__ == "__main__" else ""
