a2a62cb90ab380d4e26730244403fa3f43e43792
NIGANE <merkhtachraf163@gmail.com>
2026-02-17 18:12:51 +0000

    pushing updates

--- projs/ex2/scope_mysteries.py
+++ projs/ex2/scope_mysteries.py
@@ -1,18 +1,51 @@
+from typing import Any, Union
 
 
-# def mage_counter() -> callable:
-#     if not hasattr(mage_counter, "count")
-#         count = 0
-#     count += 1
-#     return lambda: count = count + 1
-
-def test():
+def mage_counter() -> callable:
     x = 0
 
-    def print_x():
+    def counter() -> int:
         nonlocal x
         x += 1
-        print(x)
-    return print_x
-test()()
-test()()
+        return x
+    return counter
+
+
+def spell_accumulator(initial_power: int) -> callable:
+    x = initial_power
+
+    def accumilate(pw: int = 0) -> int:
+        nonlocal x
+        x += pw
+        return x
+    return accumilate
+
+
+def enchantment_factory(enchantment_type: str) -> callable:
+    def re(n: str) -> str:
+        return f"{enchantment_type} {n}"
+    return re
+
+
+def memory_vault() -> dict[str, callable]:
+    database = {}
+
+    def store(key: Union[str, tuple, int, ], val: Any) -> None:
+        print("called")
+        if isinstance(key, (list, dict)):
+            print("true")
+        nonlocal database
+        # database[key] = val
+
+    def recall() -> Any:
+        pass
+
+    return {
+        'store': store,
+        'recall': recall
+    }
+
+mem = memory_vault()
+mem['store'](2, "ahcraf")
+
+
