328862c0d8e103db76c56971469961d329411463
Achraf Merkht <amerkht@c4r2p3.1337.ma>
2026-03-16 10:53:55 +0000

    NEGANE

--- projs/main.c
+++ projs/main.c
@@ -6,7 +6,7 @@
 /*   By: amerkht <amerkht@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 15:12:20 by amerkht           #+#    #+#             */
-/*   Updated: 2026/03/15 23:48:20 by amerkht          ###   ########.fr       */
+/*   Updated: 2026/03/16 09:45:33 by amerkht          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,32 @@
 #define sep "//////////\n"
 #include <pthread.h>
 
+pthread_t tid[2];
+int counter;
+pthread_mutex_t lock;
 
-void *fn(void *)
-{
-	printf("hello inside the thread");
+void* trythis(void* arg) {
+	(void) arg;
+    counter += 1;
+    printf("Job %d started\n", counter);
+	counter++;
+    printf("Job %d finished\n", counter);
+    return NULL;
+
 }
 
-int	main(void)
-{
-	pthread_t pth;
-	
-	pthread_create(pth, NULL, fn, NULL);
-	pthread_join(pth, NULL);
-}
+int main() {
+
+    pthread_mutex_init(&lock, NULL);
+    for (int i = 0; i < 2; i++)
+	{
+        pthread_create(&tid[i], NULL, trythis, NULL);
+	}
+	pthread_mutex_lock(&lock);
+	pthread_join(tid[0], NULL);
+    pthread_mutex_unlock(&lock);
+    pthread_join(tid[1], NULL);
+    pthread_mutex_destroy(&lock);
+    return 0;
+
+} 
