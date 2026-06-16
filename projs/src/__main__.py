5827ad8d9821c07a9ceff5aef4af77dcfaf1be12
Achraf Merkht <amerkht@c4r2p4.1337.ma>
2026-06-16 19:05:06 +0100

    NEGANE

--- projs/src/__main__.py
+++ projs/src/__main__.py
@@ -1,6 +1,22 @@
 import sys
 from src.models.Parser import Parser
+from src.models.Error import MyError
+
+
+def _usage() -> str:
+    return "usage :uv run python -m src <FILE_PATH>"
 
 
 def main() -> None:
-    parsed_data = Parser(sys.argv)
+
+    try:
+        if (len(sys.argv) != 2):
+            raise MyError(f"Error: {_usage()}")
+        parsed_data = Parser(sys.argv)
+    except MyError as e:
+        print(f"# {e}")
+    
+
+
+if __name__ == "__main__":
+    main()
--- projs/src/models/Parser.py
+++ projs/src/models/Parser.py
@@ -1,6 +1,29 @@
 from typing import List
+from src.models.Error import MyError
 
 
 class Parser():
-    def __init__(self, argv: List[str]):
+    def __init__(self, argv: List[str]) -> None:
         self.argv = argv
+        self.load_file()
+
+    def load_file(self):
+        data = []
+        try:
+            with open(self.argv[1], "r") as f:
+                data = f.readlines()
+            self.run_validation(data)
+        except FileNotFoundError as e:
+            raise MyError(f"FileNotFound {e}")
+
+    def run_validation(self, data):
+        for line in data:
+            line = line.strip()
+            if line.startswith("#") or not line:
+                continue
+            if line.startswith("nb_drones"):
+                self.validator.nb_drones(line)
+            if line.startswith("start_hub") or line.startswith("end_hub") or line.startswith("hub"):
+                self.validator.hubs(line)
+            if line.startswith("connection"):
+                self.validator.connection(line)
